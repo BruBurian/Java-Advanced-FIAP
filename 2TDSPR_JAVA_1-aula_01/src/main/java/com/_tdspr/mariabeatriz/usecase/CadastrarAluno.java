972b94694e88d950ae325e703649f479339b1b1c
Bruno Nogueira Burian <151679524+BruBurian@users.noreply.github.com>
2024-08-21 15:51:57 -0300

    Delete 2TDSPR_JAVA_1-aula_01 directory

--- 2TDSPR_JAVA_1-aula_01/src/main/java/com/_tdspr/mariabeatriz/usecase/CadastrarAluno.java
+++
@@ -1,9 +0,0 @@
-package com._tdspr.mariabeatriz.usecase;
-
-import com._tdspr.mariabeatriz.domains.Aluno;
-
-public interface CadastrarAluno {
-
-    public Aluno executar(Aluno alunoParaCadastrar);
-
-}
--- 2TDSPR_JAVA_1-aula_01/src/main/java/com/_tdspr/mariabeatriz/usecase/CadastrarProfessor.java
+++
@@ -1,9 +0,0 @@
-package com._tdspr.mariabeatriz.usecase;
-
-import com._tdspr.mariabeatriz.domains.Professor;
-
-public interface CadastrarProfessor {
-
-    public Professor executar(Professor professorParaCadastrar);
-
-}
